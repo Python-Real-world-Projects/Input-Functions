6c492b372033452f7f16449254a06254e1a8af79
Arunponugotii <arunponugoti2565@gmail.com>
2023-08-30 23:39:11 +0530

    variables

---
+++ Day-1/variables.py
@@ -0,0 +1,5 @@
+# Varaibles are nothing but a representator or like a title for the collections words
+# Instead of calling all the words ,you can simply call vriables.
+# In simple terms variables are simplifies the things
+name = "Arun"
+print(name)
